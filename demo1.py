ab6031673f73f6c455b2ca699c33b86cd827afe1
rajuf6 <rajupanugantif6@gmail.com>
2020-08-30 20:21:33 +0530

    https://github.com/rajupanuganti/xlsx.git

---
+++ demo1.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+
+excel1 = 'test1.xlsx'
+excel2 = 'test2.xlsx'
+
+df1 = pd.read_excel(excel1)
+df2 = pd.read_excel(excel2)
+
+values1 = df1[['city','km']]
+values2 = df2[['city','km']]
+
+dataframes = [values1, values2]
+
+join = pd.concat(dataframes)
+
+join.to_excel('ressult.xlsx')
