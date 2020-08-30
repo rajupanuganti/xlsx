42be05a6fe52b3bcd1077e32f3f7639950dd7f34
rajuf6 <rajupanugantif6@gmail.com>
2020-08-30 20:31:23 +0530

    sheet

--- demo1.py
+++ demo1.py
@@ -4,7 +4,7 @@ excel1 = 'test1.xlsx'
 excel2 = 'test2.xlsx'
 
 df1 = pd.read_excel(excel1)
-df2 = pd.read_excel(excel2)
+df2 = pd.read_excel(excel2) 
 
 values1 = df1[['city','km']]
 values2 = df2[['city','km']]
